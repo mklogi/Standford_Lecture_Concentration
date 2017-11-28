ded189e8c0f8d40e4df23eb60ec91bca89864ba0
mklogi <mm.j.f.kowalczyk@gmail.com>
2017-11-28 10:55:28 +0100

    Initial Commit

---
+++ Standford_Lecture_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Standford_Lecture_1
+//
+//  Created by Marcin Kowalczyk on 28.11.2017.
+//  Copyright © 2017 Marcin Kowalczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
