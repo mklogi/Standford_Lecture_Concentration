6534281382a2bb8a0132636c01dbe2fbcaaa50e0
mklogi <mm.j.f.kowalczyk@gmail.com>
2017-11-28 20:27:23 +0100

    Lecture2_Stanford

--- Standford_Lecture_1/Concetration.swift
+++ Standford_Lecture_1/Concetration.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+
+class Concetration {
+    
+    var cards = [Card]()
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if card match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+       
+    }
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        
+        //TODO: Shuffle the cards
+    }
+    
+}
